767c4324e7f85ccbcd6fd39d7875418fef65e0db
Jack Cherng <jfcherng@gmail.com>
2018-12-17 21:28:47 +0800

    Prefer using "rus" as the language code
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- src/languages/ru.php
+++ src/languages/ru.php
@@ -1,7 +1,5 @@
 <?php
 
-return [
-    'old_version' => 'Старая версия',
-    'new_version' => 'Новая версия',
-    'differences' => 'Различия',
-];
+// DEPRECATED: use rus.php instead
+// just an alias, we should use 3-char language code
+return require __DIR__ . '/rus.php';
