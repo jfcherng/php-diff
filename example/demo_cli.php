129d51e35296205cc596f21aff8aaacb48e9e77c
Jack Cherng <jfcherng@gmail.com>
2020-04-06 19:58:05 +0800

    docs: add example/demo_cli.php
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ example/demo_cli.php
@@ -0,0 +1,46 @@
+<?php
+
+include __DIR__ . '/demo_base.php';
+
+use Jfcherng\Diff\DiffHelper;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Output\StreamOutput;
+
+$output = new StreamOutput(
+    \fopen('php://stdout', 'w'),
+    StreamOutput::VERBOSITY_NORMAL,
+    null,
+    new OutputFormatter(
+        false,
+        [
+            'section' => new OutputFormatterStyle('white', 'cyan', ['bold']),
+        ]
+    )
+);
+
+$output->writeln("<section>Unified Diff\n============\n</>");
+
+// generate a unified diff
+$unifiedResult = DiffHelper::calculate(
+    $oldString,
+    $newString,
+    'Unified',
+    $diffOptions,
+    $rendererOptions
+);
+
+echo $unifiedResult . "\n\n\n\n";
+
+$output->writeln("<section>Context Diff\n============\n</>");
+
+// generate a context diff
+$contextResult = DiffHelper::calculate(
+    $oldString,
+    $newString,
+    'Context',
+    $diffOptions,
+    $rendererOptions
+);
+
+echo $contextResult . "\n\n\n\n";
