e85c214b22c56d1547b6e0f6d54e4bb3b7393324
Jack Cherng <jfcherng@gmail.com>
2022-10-01 22:44:00 +0800

    style: reduce indentation for the demo codes
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- example/demo_web.php
+++ example/demo_web.php
@@ -8,230 +8,233 @@ use Jfcherng\Diff\Factory\RendererFactory;
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>jfcherng/php-diff - Examples</title>
 
-        <!-- Prism -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1/themes/prism-okaidia.min.css" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1/plugins/line-numbers/prism-line-numbers.min.css" />
+<head>
+    <meta charset="UTF-8" />
+    <title>jfcherng/php-diff - Examples</title>
 
-        <style type="text/css">
-            html {
-                font-size: 13px;
-            }
-            .token.coord {
-                color: #6cf;
-            }
-            .token.diff.bold {
-                color: #fb0;
-                font-weight: normal;
-            }
+    <!-- Prism -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1/themes/prism-okaidia.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1/plugins/line-numbers/prism-line-numbers.min.css" />
 
-            <?php echo DiffHelper::getStyleSheet(); ?>
-        </style>
-    </head>
-    <body>
-        <h1>None-level Diff</h1>
-        <?php
+    <style type="text/css">
+        html {
+            font-size: 13px;
+        }
+        .token.coord {
+            color: #6cf;
+        }
+        .token.diff.bold {
+            color: #fb0;
+            font-weight: normal;
+        }
 
-        // demo the no-inline-detail diff
-        $inlineResult = DiffHelper::calculate(
-            $oldString,
-            $newString,
-            'Inline',
-            $diffOptions,
-            ['detailLevel' => 'none'] + $rendererOptions
-        );
+        <?php echo DiffHelper::getStyleSheet(); ?>
+    </style>
+</head>
 
-        echo $inlineResult;
+<body>
+    <h1>None-level Diff</h1>
+    <?php
 
-        ?>
+    // demo the no-inline-detail diff
+    $inlineResult = DiffHelper::calculate(
+        $oldString,
+        $newString,
+        'Inline',
+        $diffOptions,
+        ['detailLevel' => 'none'] + $rendererOptions
+    );
 
-        <h1>Line-level Diff (Default)</h1>
-        <?php
+    echo $inlineResult;
 
-        // demo the word-level diff
-        $inlineResult = DiffHelper::calculate(
-            $oldString,
-            $newString,
-            'Inline',
-            $diffOptions,
-            ['detailLevel' => 'line'] + $rendererOptions,
-        );
+    ?>
 
-        echo $inlineResult;
+    <h1>Line-level Diff (Default)</h1>
+    <?php
 
-        ?>
+    // demo the word-level diff
+    $inlineResult = DiffHelper::calculate(
+        $oldString,
+        $newString,
+        'Inline',
+        $diffOptions,
+        ['detailLevel' => 'line'] + $rendererOptions,
+    );
 
-        <h1>Word-level Diff</h1>
-        <?php
+    echo $inlineResult;
 
-        // demo the word-level diff
-        $inlineResult = DiffHelper::calculate(
-            $oldString,
-            $newString,
-            'Inline',
-            $diffOptions,
-            ['detailLevel' => 'word'] + $rendererOptions
-        );
+    ?>
 
-        echo $inlineResult;
+    <h1>Word-level Diff</h1>
+    <?php
 
-        ?>
+    // demo the word-level diff
+    $inlineResult = DiffHelper::calculate(
+        $oldString,
+        $newString,
+        'Inline',
+        $diffOptions,
+        ['detailLevel' => 'word'] + $rendererOptions
+    );
 
-        <h1>Character-level Diff</h1>
-        <?php
+    echo $inlineResult;
 
-        // demo the character-level diff
-        $inlineResult = DiffHelper::calculate(
-            $oldString,
-            $newString,
-            'Inline',
-            $diffOptions,
-            ['detailLevel' => 'char'] + $rendererOptions
-        );
+    ?>
 
-        echo $inlineResult;
+    <h1>Character-level Diff</h1>
+    <?php
 
-        ?>
+    // demo the character-level diff
+    $inlineResult = DiffHelper::calculate(
+        $oldString,
+        $newString,
+        'Inline',
+        $diffOptions,
+        ['detailLevel' => 'char'] + $rendererOptions
+    );
 
-        <h1>Side by Side Diff</h1>
-        <?php
+    echo $inlineResult;
 
-        // generate a side by side diff
-        $sideBySideResult = DiffHelper::calculateFiles(
-            $oldFile,
-            $newFile,
-            'SideBySide',
-            $diffOptions,
-            $rendererOptions
-        );
+    ?>
 
-        echo $sideBySideResult;
+    <h1>Side by Side Diff</h1>
+    <?php
 
-        ?>
+    // generate a side by side diff
+    $sideBySideResult = DiffHelper::calculateFiles(
+        $oldFile,
+        $newFile,
+        'SideBySide',
+        $diffOptions,
+        $rendererOptions
+    );
 
-        <h1>Inline Diff</h1>
-        <?php
+    echo $sideBySideResult;
 
-        // generate an inline diff
-        $inlineResult = DiffHelper::calculateFiles(
-            $oldFile,
-            $newFile,
-            'Inline',
-            $diffOptions,
-            $rendererOptions
-        );
+    ?>
 
-        echo $inlineResult;
+    <h1>Inline Diff</h1>
+    <?php
 
-        ?>
+    // generate an inline diff
+    $inlineResult = DiffHelper::calculateFiles(
+        $oldFile,
+        $newFile,
+        'Inline',
+        $diffOptions,
+        $rendererOptions
+    );
 
-        <h1>Combined Diff</h1>
-        <?php
+    echo $inlineResult;
 
-        // generate a combined diff
-        $sideBySideResult = DiffHelper::calculateFiles(
-            $oldFile,
-            $newFile,
-            'Combined',
-            $diffOptions,
-            $rendererOptions
-        );
+    ?>
 
-        echo $sideBySideResult;
+    <h1>Combined Diff</h1>
+    <?php
 
-        ?>
+    // generate a combined diff
+    $sideBySideResult = DiffHelper::calculateFiles(
+        $oldFile,
+        $newFile,
+        'Combined',
+        $diffOptions,
+        $rendererOptions
+    );
 
-        <h1>Unified Diff</h1>
-        <pre><code class="language-diff line-numbers"><?php
+    echo $sideBySideResult;
 
-        // generate a unified diff
-        $unifiedResult = DiffHelper::calculateFiles(
-            $oldFile,
-            $newFile,
-            'Unified',
-            $diffOptions,
-            $rendererOptions
-        );
+    ?>
 
-        echo \htmlspecialchars($unifiedResult);
+    <h1>Unified Diff</h1>
+    <pre><code class="language-diff line-numbers"><?php
 
-        ?></code></pre>
+    // generate a unified diff
+    $unifiedResult = DiffHelper::calculateFiles(
+        $oldFile,
+        $newFile,
+        'Unified',
+        $diffOptions,
+        $rendererOptions
+    );
 
-        <h1>Context Diff</h1>
-        <pre><code class="language-diff line-numbers"><?php
+    echo \htmlspecialchars($unifiedResult);
 
-        // generate a context diff
-        $contextResult = DiffHelper::calculateFiles(
-            $oldFile,
-            $newFile,
-            'Context',
-            $diffOptions,
-            $rendererOptions
-        );
+    ?></code></pre>
 
-        echo \htmlspecialchars($contextResult);
+    <h1>Context Diff</h1>
+    <pre><code class="language-diff line-numbers"><?php
 
-        ?></code></pre>
+    // generate a context diff
+    $contextResult = DiffHelper::calculateFiles(
+        $oldFile,
+        $newFile,
+        'Context',
+        $diffOptions,
+        $rendererOptions
+    );
 
-        <?php
+    echo \htmlspecialchars($contextResult);
 
-        // change JSON output settings for better human reading
-        $rendererOptions['outputTagAsString'] = true;
-        $rendererOptions['jsonEncodeFlags'] |= \JSON_PRETTY_PRINT;
+    ?></code></pre>
 
-        ?>
+    <?php
 
-        <h1>Text JSON Diff</h1>
-        <pre><code class="language-json line-numbers"><?php
+    // change JSON output settings for better human reading
+    $rendererOptions['outputTagAsString'] = true;
+    $rendererOptions['jsonEncodeFlags'] |= \JSON_PRETTY_PRINT;
 
-        // generate a plain text JSON diff
-        $jsonResult = DiffHelper::calculateFiles(
-            $oldFile,
-            $newFile,
-            'JsonText',
-            $diffOptions,
-            $rendererOptions
-        );
+    ?>
 
-        echo \htmlspecialchars($jsonResult);
+    <h1>Text JSON Diff</h1>
+    <pre><code class="language-json line-numbers"><?php
 
-        ?></code></pre>
+    // generate a plain text JSON diff
+    $jsonResult = DiffHelper::calculateFiles(
+        $oldFile,
+        $newFile,
+        'JsonText',
+        $diffOptions,
+        $rendererOptions
+    );
 
-        <h1>HTML JSON Diff</h1>
-        <pre><code class="language-json line-numbers"><?php
+    echo \htmlspecialchars($jsonResult);
 
-        // generate a HTML JSON diff
-        $jsonResult = DiffHelper::calculateFiles(
-            $oldFile,
-            $newFile,
-            'JsonHtml',
-            $diffOptions,
-            $rendererOptions
-        );
+    ?></code></pre>
 
-        echo \htmlspecialchars($jsonResult);
+    <h1>HTML JSON Diff</h1>
+    <pre><code class="language-json line-numbers"><?php
 
-        ?></code></pre>
+    // generate a HTML JSON diff
+    $jsonResult = DiffHelper::calculateFiles(
+        $oldFile,
+        $newFile,
+        'JsonHtml',
+        $diffOptions,
+        $rendererOptions
+    );
 
-        <h1>HTML Diff from the Result of JSON Diff</h1>
-        <?php
+    echo \htmlspecialchars($jsonResult);
 
-        $jsonArray = \json_decode($jsonResult, true);
+    ?></code></pre>
 
-        $htmlRenderer = RendererFactory::make('Inline', $rendererOptions);
-        $inlineResult = $htmlRenderer->renderArray($jsonArray);
+    <h1>HTML Diff from the Result of JSON Diff</h1>
+    <?php
 
-        echo $inlineResult;
+    $jsonArray = \json_decode($jsonResult, true);
 
-        ?>
+    $htmlRenderer = RendererFactory::make('Inline', $rendererOptions);
+    $inlineResult = $htmlRenderer->renderArray($jsonArray);
 
-        <!-- Prism -->
-        <script src="https://cdn.jsdelivr.net/npm/prismjs@1/prism.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-diff.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-json.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/prismjs@1/plugins/line-numbers/prism-line-numbers.min.js"></script>
-    </body>
+    echo $inlineResult;
+
+    ?>
+
+    <!-- Prism -->
+    <script src="https://cdn.jsdelivr.net/npm/prismjs@1/prism.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-diff.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/prismjs@1/components/prism-json.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/prismjs@1/plugins/line-numbers/prism-line-numbers.min.js"></script>
+</body>
+
 </html>
