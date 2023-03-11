77d4dbb54a68270f5e9117ee0b2696e5d19097d0
Jack Cherng <jfcherng@gmail.com>
2023-03-11 15:17:07 +0800

    test: tidy codes
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- tests/IgnoreLineEndingTest.php
+++ tests/IgnoreLineEndingTest.php
@@ -18,7 +18,7 @@ final class IgnoreLineEndingTest extends TestCase
     /**
      * @return string[][]
      */
-    public function provideIgnoreLineEndingTrue(): array
+    public function provideIgnoreLineEnding(): array
     {
         return [
             [
@@ -26,16 +26,8 @@ final class IgnoreLineEndingTest extends TestCase
                 file_get_contents(__DIR__ . '/data/ignore_line_ending/new_1.txt'),
                 <<<'DIFF'
 DIFF,
+                true,
             ],
-        ];
-    }
-
-    /**
-     * @return string[][]
-     */
-    public function provideIgnoreLineEndingFalse(): array
-    {
-        return [
             [
                 file_get_contents(__DIR__ . '/data/ignore_line_ending/old_1.txt'),
                 file_get_contents(__DIR__ . '/data/ignore_line_ending/new_1.txt'),
@@ -47,31 +39,22 @@ DIFF,
 +line 2
 
 DIFF,
+                false,
             ],
         ];
     }
 
     /**
-     * @dataProvider provideIgnoreLineEndingTrue
+     * @dataProvider provideIgnoreLineEnding
      */
-    public function testIgnoreLineEndingTrue(string $old, string $new, string $expectedDiff): void
-    {
+    public function testIgnoreLineEnding(
+        string $old,
+        string $new,
+        string $expectedDiff,
+        bool $gnoreLineEnding
+    ): void {
         $diff = DiffHelper::calculate($old, $new, 'Unified', [
-            'ignoreLineEnding' => true,
-        ], [
-            'cliColorization' => RendererConstant::CLI_COLOR_DISABLE,
-        ]);
-
-        static::assertSame($expectedDiff, $diff);
-    }
-
-    /**
-     * @dataProvider provideIgnoreLineEndingFalse
-     */
-    public function testIgnoreLineEndingFalse(string $old, string $new, string $expectedDiff): void
-    {
-        $diff = DiffHelper::calculate($old, $new, 'Unified', [
-            'ignoreLineEnding' => false,
+            'ignoreLineEnding' => $gnoreLineEnding,
         ], [
             'cliColorization' => RendererConstant::CLI_COLOR_DISABLE,
         ]);
