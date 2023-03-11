e2c353a15ad82283ccabeea623f60c52bf89d3a8
Jack Cherng <jfcherng@gmail.com>
2023-03-11 15:17:07 +0800

    test: tidy codes
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- tests/IgnoreLineEndingTest.php
+++ tests/IgnoreLineEndingTest.php
@@ -18,27 +18,20 @@ final class IgnoreLineEndingTest extends TestCase
     /**
      * @return string[][]
      */
-    public function provideIgnoreLineEndingTrue(): array
+    public function provideIgnoreLineEnding(): array
     {
         return [
             [
-                file_get_contents(__DIR__ . '/data/ignore_line_ending/old_1.txt'),
-                file_get_contents(__DIR__ . '/data/ignore_line_ending/new_1.txt'),
+                \file_get_contents(__DIR__ . '/data/ignore_line_ending/old_1.txt'),
+                \file_get_contents(__DIR__ . '/data/ignore_line_ending/new_1.txt'),
                 <<<'DIFF'
 DIFF
+                ,
+                true,
             ],
-        ];
-    }
-
-    /**
-     * @return string[][]
-     */
-    public function provideIgnoreLineEndingFalse(): array
-    {
-        return [
             [
-                file_get_contents(__DIR__ . '/data/ignore_line_ending/old_1.txt'),
-                file_get_contents(__DIR__ . '/data/ignore_line_ending/new_1.txt'),
+                \file_get_contents(__DIR__ . '/data/ignore_line_ending/old_1.txt'),
+                \file_get_contents(__DIR__ . '/data/ignore_line_ending/new_1.txt'),
                 <<<"DIFF"
 @@ -1,2 +1,2 @@
 -line 1\r
@@ -47,31 +40,23 @@ DIFF
 +line 2
 
 DIFF
+                ,
+                false,
             ],
         ];
     }
 
     /**
-     * @dataProvider provideIgnoreLineEndingTrue
+     * @dataProvider provideIgnoreLineEnding
      */
-    public function testIgnoreLineEndingTrue(string $old, string $new, string $expectedDiff): void
-    {
+    public function testIgnoreLineEnding(
+        string $old,
+        string $new,
+        string $expectedDiff,
+        bool $gnoreLineEnding
+    ): void {
         $diff = DiffHelper::calculate($old, $new, 'Unified', [
-            'ignoreLineEnding' => true,
-        ], [
-            'cliColorization' => RendererConstant::CLI_COLOR_DISABLE,
-        ]);
-
-        static::assertSame($expectedDiff, $diff);
-    }
-
-    /**
-     * @dataProvider provideIgnoreLineEndingFalse
-     */
-    public function testIgnoreLineEndingFalse(string $old, string $new, string $expectedDiff): void
-    {
-        $diff = DiffHelper::calculate($old, $new, 'Unified', [
-            'ignoreLineEnding' => false,
+            'ignoreLineEnding' => $gnoreLineEnding,
         ], [
             'cliColorization' => RendererConstant::CLI_COLOR_DISABLE,
         ]);
