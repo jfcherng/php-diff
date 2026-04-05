b78eada80c6275cc7b6857015a8150b5d5091cab
Jack Cherng <jfcherng@gmail.com>
2026-04-06 07:23:31 +0800

    chore: apply php-cs-fixer after modernization refactors

--- src/Renderer/Html/LineRenderer/LineRendererInterface.php
+++ src/Renderer/Html/LineRenderer/LineRendererInterface.php
@@ -13,8 +13,6 @@ interface LineRendererInterface
      *
      * @param MbString $mbOld the old megabytes line
      * @param MbString $mbNew the new megabytes line
-     *
-     * @return static
      */
     public function render(MbString $mbOld, MbString $mbNew): static;
 }
